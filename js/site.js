713ad5ccdffb11be7b33c28642b65174b9d657f7
Jory <46983170+JorySmith@users.noreply.github.com>
2021-11-16 10:28:42 -0600

    Initial JS update

--- js/site.js
+++ js/site.js
@@ -1,4 +1,40 @@
-// Call Hello World
-function helloWorld() {
-    alert("Hello World");
+// Get function
+function userInputs() {
+    // Get user supplied loan details
+    let userLoanAmount = document.getElementById("loanAmount").value;
+    let userTotalPayments = document.getElementById("payments").value;
+    let userIntRate = document.getElementById("intRate").value;
+
+    // Calculate loan details based on inputs
+    calculateLoanDetails(userLoanAmount, userTotalPayments, userIntRate)
+
+    // Display loan details in the page/DOM
+    displayLoanDetails(loanDetails)
+}
+
+// Logic function
+function calculateLoanDetails(loanAmount, totalPayments, intRate) {
+    // Declare loanDetails object, which this function will return
+    // Return data includes totalPrincipal, totalInterest, totalCost, and amortizationTable
+    let loanDetails = {
+        totalPrincipal = 0,
+        totalInterest = 0.0,
+        totalCost = 0,
+        amortizationTable = []
+    };
+
+    // Ensure loanAmount and totalPayments parameters are numbers (data type) and integers
+    // Ensure intRate parameter is a number (data type) and a float
+    if (condition) {
+        
+    } else {
+        alert("Please enter numbers only")
+    }
+
+    return loanDetails
+}
+
+// Display function
+function displayLoanDetails(loanDetails) {
+
 }

d096b7d1e35addc760c37fcb63c46fa6b4bbdbde
Jory <46983170+JorySmith@users.noreply.github.com>
2021-11-16 14:19:52 -0600

    Added calculateMonthlyPayment function

--- js/site.js
+++ js/site.js
@@ -37,33 +37,51 @@ function calculateLoanDetails(loanAmount, totalPayments, intRate) {
     // Calculate i = monthly interest rate after converting user-supplied whole number interest rate into decimal form first
     let monthlyIntRate = (parsedIntRate/100/12);   
 
-    // Updated formula:
+    // Updated monthly mortgage payment formula:
     // M = monthlyPayment; P = parsedLoanAmount; i = monthlyIntRate; n = totalPayments
-    // monthlyPayment = parsedLoanAmount [ monthlyIntRate(1 + monthlyIntRate) ** totalPayments ] / [ (1 + monthlyIntRate) ** totalPayments – 1]  
+    // monthlyPayment = parsedLoanAmount * ((monthlyIntRate(1 + monthlyIntRate)) ** totalPayments) / (((1 + monthlyIntRate) ** totalPayments) – 1)
     
-    // Calculate monthly payment, totalInterest, and totalCost        
+    // Calculate monthly payment        
+    monthlyPayment = calculateMonthlyPayment(parsedLoanAmount, monthlyIntRate, totalPayments);
+
+    // Calculate totalInterest and totalCost
+    totalCost = (monthlyPayment.totalMonthlyPayment * totalPayments);
+    totalInterest = (totalCost - parsedLoanAmount);
+
+    // Create amortization table for as long as totalPayments requires 
+    let    
+    for (let payment = 1; payment <= totalPayments; payment++) {
+        
+        calculateMonthlyPayment(parsedLoanAmount, monthlyIntRate, totalPayments)
+        // Calculate how much of each payment reduces the principal only
+        // Calculate how much of each payment goes towards interest
+        // Track and update total interest paid after each payment
+        // Add each monthly payment data array to amortizationTable array
+        // Track and update reduced loan balance after each payment
+    }
+    
+    return loanDetails
+}
+
+// Calculate monthly payment
+// Return total monthly payment, monthly interest payment, and monthly principal payment
+function calculateMonthlyPayment(parsedLoanAmount, monthlyIntRate, totalPayments) {
+    // Declare return object
+    let returnObj = {
+        totalMonthlyPayment: 0,
+        monthlyInterestPayment: 0,
+        monthlyPrincipalPayment: 0,
+    }    
+    // Step through monthly mortgage payment formula
     let resultA = monthlyIntRate * ((1 + monthlyIntRate) ** totalPayments);
     let resultB = (((1 + monthlyIntRate) ** totalPayments) - 1);
     let resultC = (resultA/resultB);
-    monthlyPayment = (parsedLoanAmount * resultC);
-    totalCost = (monthlyPayment * totalPayments);
-    totalInterest = (totalCost - parsedLoanAmount);
+    totalMonthlyPayment = (parsedLoanAmount * resultC);
 
-    // Create amortization table
-    // Calculate each monthly payment for as long as totalPayments requires
-    // Calculate how much of each payment reduces the principal only
-    // Calculate how much of each payment goes towards interest
-    for (let payment = 1; payment <= totalPayments; payment++) {
-        
-    }
-    // Add each monthly payment data array to amortizationTable array
-    // Track and update total interest paid/totalInterest after each payment
-    // Track and update new loan balance/new parsedLoanAmount after each payment
-    // Calculate and update totalCost (totalInterest + parsedLoanAmount) 
-
-    
-
-    return loanDetails
+    // Monthly interest payment formula: loan balance * monthlyIntRate
+    monthlyInterestPayment = (parsedLoanAmount * monthlyIntRate);
+    // Monthly principal payment formula: monthly payment - monthly interest payment
+    monthlyPrincipalPayment = (totalMonthlyPayment - monthlyInterestPayment);
 }
 
 // Display function

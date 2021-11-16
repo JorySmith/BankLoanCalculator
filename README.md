92e950c8f74ffbbb0a1bc8a89dd806e81f666e4c
Jory <46983170+JorySmith@users.noreply.github.com>
2021-11-16 13:35:48 -0600

    Calculated monthly payment, totalInterest, and totalCost

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Bank Loan Calculator - A Coding Project
-A JavaScript program that displays an amortization table for a home loan based on user defined loan amount, total payments, and interest rate.
+A JavaScript program that displays an amortization table for a home loan based on a user-defined loan amount, total payments, and interest rate.
 
 # Live Demo
 https://jorysmith.github.io/BankLoanCalculator/
--- js/site.js
+++ js/site.js
@@ -17,7 +17,7 @@ function calculateLoanDetails(loanAmount, totalPayments, intRate) {
     // Declare loanDetails object, which this function will return
     // Return data should include totalPrincipal, totalInterest, totalCost, and amortizationTable
     let loanDetails = {
-        totalPrincipal: 0,
+        totalPrincipal: loanAmount,
         totalInterest: 0.0,
         totalCost: 0,
         amortizationTable: []
@@ -32,19 +32,34 @@ function calculateLoanDetails(loanAmount, totalPayments, intRate) {
     // Monthly mortgage payment formula: M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1]    
     // M = monthly payment; P = principal loan amount; i = monthly interest rate; n = number of months required to repay the loan
 
-    // Calculate i = monthly interest rate
-    let monthlyIntRate = (intRate/12);
+    // Declare monthly payment - M
+    let monthlyPayment = 0; 
+    // Calculate i = monthly interest rate after converting user-supplied whole number interest rate into decimal form first
+    let monthlyIntRate = (parsedIntRate/100/12);   
 
-    // Updated formula: M = monthly payment; P = parsedLoanAmount; i = monthlyIntRate; n = totalPayments
+    // Updated formula:
+    // M = monthlyPayment; P = parsedLoanAmount; i = monthlyIntRate; n = totalPayments
+    // monthlyPayment = parsedLoanAmount [ monthlyIntRate(1 + monthlyIntRate) ** totalPayments ] / [ (1 + monthlyIntRate) ** totalPayments – 1]  
+    
+    // Calculate monthly payment, totalInterest, and totalCost        
+    let resultA = monthlyIntRate * ((1 + monthlyIntRate) ** totalPayments);
+    let resultB = (((1 + monthlyIntRate) ** totalPayments) - 1);
+    let resultC = (resultA/resultB);
+    monthlyPayment = (parsedLoanAmount * resultC);
+    totalCost = (monthlyPayment * totalPayments);
+    totalInterest = (totalCost - parsedLoanAmount);
 
-    // Create amortization table, assign to amortizationTable
-    // Calculate each month's payment for as long as totalPayments requires
-    // For each monthly payment:
+    // Create amortization table
+    // Calculate each monthly payment for as long as totalPayments requires
     // Calculate how much of each payment reduces the principal only
     // Calculate how much of each payment goes towards interest
+    for (let payment = 1; payment <= totalPayments; payment++) {
+        
+    }
+    // Add each monthly payment data array to amortizationTable array
     // Track and update total interest paid/totalInterest after each payment
     // Track and update new loan balance/new parsedLoanAmount after each payment
-    // Calculate and update total cost/totalCost (totalInterest + parsedLoanAmount)       
+    // Calculate and update totalCost (totalInterest + parsedLoanAmount) 
 
     
 
